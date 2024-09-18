9d8b8ac42011148729fefc90452e8e2386cce620
David Phume <davidphume@gmail.com>
2024-09-18 18:22:24 +0200

    my 2nd commit

---
+++ map/main.cpp
@@ -0,0 +1,39 @@
+
+//Afrobotics.com 2024.
+//Author : David Phume.
+// This code is a 3d map of the robot environment.
+
+// Main concept of Programming
+// Class - A class is a block of code from which an objects can be created from, it can also be thought of a custom data type.
+// Object - An object is an instance of a class. It can also be thought of as a variable with a custom data type.
+// Polymorphsm is when a dirived class uses functionalities from the base class in it's own way.
+// inheritance is when a dirived class inherits functionalities from the base class.
+// Abstraction is a concept in programming where the unnessessary details are hidden and funtionality is simplified.
+// Encapsulation is a concept in object oriented programming used to hide and protect data.
+// Composition is when a class is used within another class.
+//
+
+
+
+
+#include <iostream>
+using namespace std;
+
+/*
+class animal{
+
+public:
+
+};
+
+
+*/
+
+
+
+int main (){
+
+cout << "hello robot" << endl;
+
+    return 0;
+}
