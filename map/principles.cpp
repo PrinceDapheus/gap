948fadcad68f91ad56b128a0b0a4106e97978420
David Phume <davidphume@gmail.com>
2024-09-19 23:01:24 +0200

    my 5th commit - ran priciples file as the comments were not showing on github

---
+++ map/principles.cpp
@@ -0,0 +1,62 @@
+
+
+// Afrobotics.com
+// Author : David Phume
+
+/*
+---- CORE FIRST PRINCIPLES OF C++ -----
+
+1. CORE STRUCTURE, SYNTAX 
+BASIC BUILDING BLOCKS
+- Variables 
+- Data types : int, string , double , float , bool etc.
+- Operators : + , - , = , < , % etc.
+
+
+CONTROL FLOW
+-  loops , if else statements, switch statements, Jump statement
+
+FUNCTIONS
+
+IN/OUT STREAMS
+
+-----------
+2. CORE CONCEPTS OF OBJECT ORIENTED PROGRAMMING
+- Classes and Objects
+- Encasulation
+- Abstaraction
+- Inheritance
+- Polymophism
+-------------
+3. ADVANCED CONCEPTS
+- Templates
+- Memory handling
+- Error handling
+- Multi-threading
+---------------
+4. LIBRARIES
+- Standard Library
+- Lambda
+
+
+
+
+
+
+
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main () {
+
+
+cout << " hello Billionare !!!" <<endl;
+
+
+    return 0;
+}
